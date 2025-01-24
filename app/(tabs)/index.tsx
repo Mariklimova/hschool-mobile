cc51e18836c84e123e41ad095c4c6e2065099192
Mariklimova <mariku22@yandex.ru>
2025-01-24 23:36:22 +0300

    added question 43 JS

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,44 @@
-import { TouchableOpacity, StyleSheet, Image, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Image, View, TextInput } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
 import { ThemedText } from '@/components/ThemedText';
 import topicImageMap from '@/utils/topicImageMap';
+import { useState, useEffect } from 'react';
+import storage from '@/storage/index.json';
+import { iDescription, iTopic } from '@/interfaces/index';
 
 export default function HomeScreen() {
   // const topics = ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'React', 'SQL', 'noSQL', 'Node.js'];
-  const topics = ['HTML', 'CSS', 'JavaScript', 'SQL', 'Node.js', 'React', 'TypeScript'];
-  const router = useRouter()
+  const topics = ['HTML', 'CSS', 'JavaScript', 'SQL', 'Node.js', 'React', 'TypeScript', 'noSQL'];
+  const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState(''); // Изменен тип состояния на строку
+  const [matchedQuestions, setMatchedQuestions] = useState<iDescription[]>([]); // Для хранения совпадений
 
-  const handleTopicPress = (topic: any) => {
+  const handleTopicPress = (topic: string) => {
     router.navigate(topic);
   };
+
+  const handleSearch = () => {
+    const topicData = storage[searchQuery];
+    if (topicData) {
+      const results = topicData.description.filter((el:iDescription) =>
+        el.question.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setMatchedQuestions(results); 
+      if (results.length > 0) {
+        router.navigate(results[0].link[0]);
+      } else {
+        console.log('Нет совпадений'); 
+      }
+    } else {
+      console.log('Нет совпадений2'); 
+    }
+  };
+
+  useEffect(() => {
+   
+    setMatchedQuestions(storage[topics[0]].description); 
+  }, []);
 
   return (
     <LinearGradient
@@ -23,6 +50,15 @@ export default function HomeScreen() {
 
       <View style={{ justifyContent: 'center', alignItems: 'center', gap: 10 }}>
         <ThemedText type='title'>Выберите тему:</ThemedText>
+
+        
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Поиск вопросов..."
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+          onSubmitEditing={handleSearch}
+        />
 
         <View style={styles.topicList}>
           {topics.map((topic, index) => (
@@ -48,6 +84,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  searchInput: {
+      width: '80%',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+
 
   topicList: {
     flexDirection: 'row',
